0d2c24f0df2713356091ec8320e5270108205c76
PepeTru3no <jvinetconsola@gmail.com>
2024-10-23 18:32:56 -0300

    desafio apis js
    empaquetado desfio metodos de arreglos y subida de desafio api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,86 @@
-const tareas = [{
-    id: 1,
-    descripcion: "Revisar fichas",
-    completada: false,
-},
-{
-    id: 2,
-    descripcion: "Cambiar precios",
-    completada: true,
-},
-{
-    id: 3,
-    descripcion: "Medir aceite",
-    completada: false,
-}];
+const api = "https://mindicador.cl/api/";
+const dropTipo = document.getElementById('dropTipo');
+const btnBuscar = document.getElementById('btnBuscar');
+const montoClp = document.getElementById('montoClp');
+const divResultado = document.getElementById('resultado');
+const divGrafico = document.getElementById('divGrafico');
 
-const muestraTareas = document.getElementById('muestraTareas');
-const nuevaTarea = document.getElementById('nuevaTarea');
-const botonAgregar = document.getElementById("botonAgregar");
-const total = document.getElementById("total");
-const realizados = document.getElementById("realizados");
-let htmlTabla = ``;
-
-function agregarTarea(tarea) {
-    let ultimoId = (tareas.length > 0) ? tareas[tareas.length - 1].id : 0;
-    tareas.push({ id: ultimoId + 1, descripcion: tarea, completada: false });
-    htmlTabla = ``;
-    actualizarTabla();
-}
-
-function eliminarTarea(id) {
-    const index = tareas.findIndex((ele) => ele.id == id)
-    tareas.splice(index, 1);
-    htmlTabla = ``;
-    actualizarTabla();
-}
-
-function cambiaEstado(id) {
-    for (let tarea of tareas) {
-        if (tarea.id === id && tarea.completada) {
-            tarea.completada = false;
-        } else if (tarea.id === id && !tarea.completada) {
-            tarea.completada = true;
-        }
+async function getApi(endpoint = "") {
+    try {
+        const res = await fetch(api + endpoint)
+        const data = await res.json()
+        return data;
+    } catch (e) {
+        alert(`El servicio no esta disponible. Error: ${e.message}`)
+        location.reload();
     }
-    htmlTabla = ``;
-    actualizarTabla();
 }
 
-function actualizarTabla() {
-    let completadas = 0;
-    for (let tarea of tareas) {
-        let color;
-        let checked;
-        if (tarea.completada) {
-            color = "table-success";
-            checked = "checked";
-            completadas++;
-        } else {
-            color = "table-danger";
-        }
-        htmlTabla += ` <tr class="${color}">
-                        <td>${tarea.id}</td>
-                        <td>${tarea.descripcion}</td>
-                        <td><input type="checkbox" id="tarea_${tarea.id}" onChange="cambiaEstado(${tarea.id})" name="tarea_${tarea.id}" ${checked}></td>
-                        <td><a href="#" onCLick="eliminarTarea(${tarea.id})"><i class="fa-solid fa-x"></i></a></td>
-                    </tr>`;
+async function getTipoMonedas() {
+    const data = await getApi();
+    html = `<option value="0">Seleccione Moneda...</option>`;
+    for (let i in data) {
+        if (data[i].nombre)
+            html += `<option value="${data[i].codigo}">${data[i].nombre}</option>`;
     }
-    realizados.innerHTML = completadas
-    total.innerHTML = tareas.length;
-    muestraTareas.innerHTML = htmlTabla;
-
+    dropTipo.innerHTML = html;
 }
-actualizarTabla();
-botonAgregar.addEventListener("click", function () {
-    if (nuevaTarea.value === "") {
-        alert("el campo no debe estar vacio")
+
+async function getResultado() {
+    if (dropTipo.value != 0 && montoClp.value != 0) {
+        const data = await getApi(dropTipo.value);
+        html = `<h3>Resultado: <i class="fa-solid fa-${dropTipo.value}-sign"></i> </h3>`;
+        resultado = (montoClp.value / data.serie[0].valor).toFixed(2);
+        html += `<h4>${resultado}.-  ${data.nombre}</h4>`;
+        html += `<p>Valor de cambio: $ ${data.serie[0].valor}.- ${data.unidad_medida}</p>`;
+        divResultado.innerHTML = html;
     } else {
-        agregarTarea(nuevaTarea.value);
-        nuevaTarea.value = "";
+        alert("Debe seleccionar una moneda o no agrego una cantidad.")
     }
-})
+}
+
+async function getAndCreateDataToChart(moneda = "") {
+    const dataApi = await getApi(moneda)
+    const labels = dataApi['serie'].map((s) => {
+        return s.fecha.slice(0, 10);
+    });
+    const data = dataApi.serie.map((s) => {
+        return Number(s.valor);
+    });
+    const datasets = [
+        {
+            label: dataApi.nombre,
+            borderColor: "rgb(255, 99, 132)",
+            data
+        }
+    ];
+    return { labels, datasets };
+}
+
+async function renderGrafica(moneda = "") {
+    const data = await getAndCreateDataToChart(moneda);
+    const config = {
+        type: "line",
+        data
+    };
+    if (!document.getElementById('grafico')) {
+        const newCanvas = document.createElement('canvas');
+        newCanvas.id = 'grafico'
+        divGrafico.appendChild(newCanvas);
+    } else {
+        divGrafico.removeChild(document.getElementById('grafico'));
+        const newCanvas = document.createElement('canvas');
+        newCanvas.id = 'grafico'
+        divGrafico.appendChild(newCanvas);
+    }
+    const myChart = document.getElementById('grafico');
+    myChart.style.backgroundColor = "white";
+    new Chart(myChart, config);
+}
+
+
+btnBuscar.addEventListener('click', function () {
+    getResultado();
+    renderGrafica(dropTipo.value);
+});
+getTipoMonedas();
